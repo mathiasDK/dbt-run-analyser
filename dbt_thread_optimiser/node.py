d6b75a944a62c7948b034c43e1566363ed4b6161
Mathias Nørskov <bondnoerskov@hotmail.com>
2025-01-14 21:51:14 +0100

    More testing

--- dbt_thread_optimiser/node.py
+++ dbt_thread_optimiser/node.py
@@ -5,52 +5,52 @@ class Node:
         self.parents = parents
         self.children = children
 
-    @property
-    def name(self):
-        return self.name
+    # @property
+    # def name(self):
+    #     return self.name
     
-    @name.setter
-    def name(self, name):
-        self.name = name
+    # @name.setter
+    # def name(self, name):
+    #     self.name = name
 
-    @name.deleter
-    def name(self):
-        raise ValueError("A node must have a name.")
+    # @name.deleter
+    # def name(self):
+    #     raise ValueError("A node must have a name.")
 
-    @property
-    def run_time(self):
-        return self.run_time
+    # @property
+    # def run_time(self):
+    #     return self.run_time
     
-    @run_time.setter
-    def run_time(self, run_time):
-        self.run_time = run_time
+    # @run_time.setter
+    # def run_time(self, run_time):
+    #     self.run_time = run_time
 
-    @run_time.deleter
-    def run_time(self):
-        print(f"{self.name} should have a run_time.")
-        del self.run_time
+    # @run_time.deleter
+    # def run_time(self):
+    #     print(f"{self.name} should have a run_time.")
+    #     del self.run_time
 
-    @property
-    def parents(self):
-        return self.parents
+    # @property
+    # def parents(self):
+    #     return self.parents
     
-    @parents.setter
-    def parents(self, parents):
-        self.parents = parents
+    # @parents.setter
+    # def parents(self, parents):
+    #     self.parents = parents
 
-    @parents.deleter
-    def parents(self):
-        print(f"{self.name} has no upstream dependencies anymore.")
-        del self.parents
+    # @parents.deleter
+    # def parents(self):
+    #     print(f"{self.name} has no upstream dependencies anymore.")
+    #     del self.parents
         
-    @property
-    def children(self):
-        return self.children
+    # @property
+    # def children(self):
+    #     return self.children
     
-    @parents.setter
-    def children(self, children):
-        self.children = children
+    # @children.setter
+    # def children(self, children):
+    #     self.children = children
 
-    @children.deleter
-    def children(self):
-        del self.children
+    # @children.deleter
+    # def children(self):
+    #     del self.children
