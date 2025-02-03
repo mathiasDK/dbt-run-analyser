3d54957f4266448e30ba94ca96e9d99e26cd3075
Mathias Nørskov <bondnoerskov@hotmail.com>
2025-02-03 17:30:32 +0100

    Adding pretty naming

--- dbt_run_analyser/utils/manifest_parser.py
+++ dbt_run_analyser/utils/manifest_parser.py
@@ -5,12 +5,22 @@ def manifest_parser(path_to_manifest)->dict:
     d = json.load(open(path_to_manifest))
 
     nodes = {}
+    name_lookup = {}
 
     for node, vals in d["nodes"].items():
+        node_name = vals.get("name")
+        name_lookup[node] = node_name
         upstream_models = vals.get("depends_on")["nodes"]
         if upstream_models == []:
-            nodes[node] = None
+            nodes[node_name] = None
         else:
-            nodes[node] = upstream_models
+            nodes[node_name] = upstream_models
+
+    for node, parents in nodes.items():
+        if parents is not None:
+            pretty_names = []
+            for parent in parents:
+                pretty_names.append(name_lookup.get(parent, parent))
+            nodes[node] = pretty_names
 
     return nodes
