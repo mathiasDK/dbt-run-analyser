f5aabedcba6120909c84c441ad9a01726801cdd0
mathiasDK <44413262+mathiasDK@users.noreply.github.com>
2025-02-03 17:31:03 +0100

    Parse manifest (#4)
    * initial parsing of manifest
    
    * Adding pretty naming
    
    ---------
    
    Co-authored-by: Mathias Nørskov <bondnoerskov@hotmail.com>

--- dbt_run_analyser/utils/manifest_parser.py
+++ dbt_run_analyser/utils/manifest_parser.py
@@ -1,16 +1,26 @@
-class ManifestParser:
-    def __init__(self) -> None:
-        self.nodes = None
+import json
 
-    @property
-    def nodes(self):
-        return self.nodes
-    
-    @nodes.setter
-    def nodes(self, nodes: list[str]):
-        self.nodes = nodes
+def manifest_parser(path_to_manifest)->dict:
+    # Convert json to dict
+    d = json.load(open(path_to_manifest))
 
-    @nodes.deleter
-    def nodes(self):
-        print("Removing all nodes")
-        del self.nodes
+    nodes = {}
+    name_lookup = {}
+
+    for node, vals in d["nodes"].items():
+        node_name = vals.get("name")
+        name_lookup[node] = node_name
+        upstream_models = vals.get("depends_on")["nodes"]
+        if upstream_models == []:
+            nodes[node_name] = None
+        else:
+            nodes[node_name] = upstream_models
+
+    for node, parents in nodes.items():
+        if parents is not None:
+            pretty_names = []
+            for parent in parents:
+                pretty_names.append(name_lookup.get(parent, parent))
+            nodes[node] = pretty_names
+
+    return nodes
