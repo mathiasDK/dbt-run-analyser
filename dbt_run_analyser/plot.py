16820c920507da5661ca10ec8b702003545952cb
Mathias Nørskov <mat@lunar.app>
2025-02-11 21:02:16 +0100

    Adding plot functionality

--- dbt_run_analyser/plot.py
+++ dbt_run_analyser/plot.py
@@ -4,7 +4,7 @@ import plotly.graph_objects as go
 class ShowDBTRun(DAG):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.figure = go.Figure
+        self.figure = go.Figure()
 
     def plot_run_time(self, title:str=None, run_time_starting_point: int|float=0, run_time_highlight: int|float=1e6, run_time_show_model_name: int|float=1e6):
         if len(self.df)==0:
@@ -15,9 +15,10 @@ class ShowDBTRun(DAG):
             if start >= run_time_starting_point:
                 end = row["relative_end_time"].total_seconds()
                 thread = row["thread"]
-                fillcolor = "grey" if row["run_time"].total_seconds()<run_time_highlight else "red"
-                model_name = "" if row["run_time"].total_seconds()<run_time_show_model_name else row["model_name"]
-                self.__add_run_time(thread=thread, start=start, end=end, fillcolor=fillcolor, model_name=model_name)
+                fillcolor = "grey" if row["run_time"]<run_time_highlight else "red"
+                show_model_name = True if row["run_time"]<run_time_show_model_name else False
+                model_name = row["model_name"]
+                self._add_run_time(thread=thread, start=start, end=end, fillcolor=fillcolor, model_name=model_name, show_model_name=show_model_name)
         
         # Layout
         self.figure.update_layout(
@@ -27,9 +28,33 @@ class ShowDBTRun(DAG):
             title=title
         )
         return self.figure
+    
+    def plot_critical_path(self, critical_path_model:str, *args, **kwargs):
+        self.plot_run_time(*args, **kwargs)
+        d = self.get_critial_path(critical_path_model)
+        for _, v in d.items():
+            nodes = v["path"]
+            break
+        for node in nodes:
+            self._highlight_node(node)
+        return self.figure
+    
+    def _highlight_node(self, node:str)->None:
+        HIGHLIGHT_COLOR = "red"
+        
+        self.figure.update_shapes(
+            selector=dict(name=node),
+            fillcolor=HIGHLIGHT_COLOR,
+            label=dict(
+                text=node, 
+                font=dict(size=10),
+            ),
+        )
+        
 
-    def _add_run_time(self, thread, start, end, fillcolor, model_name:str="")->None:
+    def _add_run_time(self, thread, start, end, fillcolor, model_name:str="", show_model_name:bool=True)->None:
         self.figure.add_shape(
+            name=model_name,
             type="rect",
             x0=start,
             x1=end,
@@ -37,7 +62,7 @@ class ShowDBTRun(DAG):
             y1=thread+0.35,
             fillcolor=fillcolor,
             label=dict(
-                text=model_name, 
+                text=model_name if show_model_name else "", 
                 font=dict(size=10),
             ),
         )
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -7,7 +7,8 @@ class DAGTest(unittest.TestCase):
 
     def test_no_data(self):
         s = ShowDBTRun()
-        self.assertRaises(Exception, s.plot_run_time())
+        with self.assertRaises(Exception):
+            s.plot_run_time()
     
     def test_add_run_time(self):
         s = ShowDBTRun()
@@ -41,5 +42,40 @@ class DAGTest(unittest.TestCase):
             "relative_end_time": [td(seconds=7, milliseconds=210), td(seconds=23, milliseconds=210), td(seconds=38, milliseconds=210), td(seconds=50, milliseconds=210)],
             "thread": [0, 0, 0, 1]
         })
-        s.plot_run_time(run_time_starting_point=td(seconds=10))
+        s.plot_run_time(run_time_starting_point=10)
         self.assertEqual(3, len(s.figure.layout.shapes))
+
+    def test_highlight_node(self):
+        s = ShowDBTRun(
+            manifest_path="test_data/manifest/manifest.json", 
+            log_file="test_data/cli_output/dbt_1_thread.log"
+        )
+        s.df = pl.DataFrame(data={
+            "model_name": ["e_order_event_7","stg_order","fct_order","order_wide"],
+            "run_time": [7.21, 10.21, 4.5, 12.33],
+            "relative_start_time": [td(seconds=0), td(seconds=13), td(seconds=33, milliseconds=710), td(seconds=37, milliseconds=880)],
+            "relative_end_time": [td(seconds=7, milliseconds=210), td(seconds=23, milliseconds=210), td(seconds=38, milliseconds=210), td(seconds=50, milliseconds=210)],
+            "thread": [0, 0, 0, 1]
+        })
+        fig = s.plot_run_time()
+        s._highlight_node("e_order_event_7")
+        actual_shape_colors = [shape.fillcolor for shape in fig.layout.shapes]
+        expected_shape_colors = ["red", "grey", "grey", "grey"]
+        self.assertEqual(expected_shape_colors, actual_shape_colors)
+
+    def test_highlight_critical_path(self):
+        s = ShowDBTRun(
+            manifest_path="test_data/manifest/manifest.json", 
+            log_file="test_data/cli_output/dbt_1_thread.log"
+        )
+        s.df = pl.DataFrame(data={
+            "model_name": ["e_order_event_7","stg_order","fct_order","order_wide"],
+            "run_time": [7.21, 10.21, 4.5, 12.33],
+            "relative_start_time": [td(seconds=0), td(seconds=13), td(seconds=33, milliseconds=710), td(seconds=37, milliseconds=880)],
+            "relative_end_time": [td(seconds=7, milliseconds=210), td(seconds=23, milliseconds=210), td(seconds=38, milliseconds=210), td(seconds=50, milliseconds=210)],
+            "thread": [0, 0, 0, 1]
+        })
+        s.plot_critical_path("e_order_event_7")
+        actual_shape_colors = [shape.fillcolor for shape in s.figure.layout.shapes]
+        expected_shape_colors = ["red", "grey", "grey", "grey"]
+        self.assertEqual(expected_shape_colors, actual_shape_colors)
